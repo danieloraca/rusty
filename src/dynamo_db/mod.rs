072e8405faaf82b423fea28ecd5800b65ab1ea40
Daniel Oraca <danieloraca@users.noreply.github.com>
2023-12-09 19:27:07 +0000

    use modules

---
+++ src/dynamo_db/mod.rs
@@ -0,0 +1,41 @@
+use aws_sdk_dynamodb::{
+    Client, Error,
+};
+
+use aws_sdk_dynamodb::types::{
+    AttributeValue,
+};
+
+use aws_config::BehaviorVersion;
+
+use uuid::Uuid;
+
+pub async fn perform_dynamodb_operations() -> Result<(), Error> {
+    let shared_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let client = Client::new(&shared_config);
+    let res = client.list_tables().send().await?;
+
+    println!("Tables: {:?}", res.table_names);
+
+    let id = Uuid::new_v4();
+    //write item to DynamoDan table
+    let request = client
+        .put_item()
+        .table_name("DynamoDan")
+        .item(
+            "id",
+            AttributeValue::S(String::from(
+                id,
+            )),
+        )
+        .item(
+            "ageR",
+            AttributeValue::N(String::from(
+                "122".to_string(),
+            )),
+        )
+        .item("name", AttributeValue::S(String::from("Dan")));
+    request.send().await?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,6 @@
-use aws_sdk_dynamodb::{
-    Client, Error,
-};
-use aws_sdk_dynamodb::types::{
-    AttributeValue,
-};
-use aws_config::BehaviorVersion;
-use uuid::Uuid;
+mod dynamo_db;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let shared_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
-    let client = Client::new(&shared_config);
-    let res = client.list_tables().send().await?;
-
-    println!("Tables: {:?}", res.table_names);
-
-    let id = Uuid::new_v4();
-    //write item to DynamoDan table
-    let request = client
-        .put_item()
-        .table_name("DynamoDan")
-        .item(
-            "id",
-            AttributeValue::S(String::from(
-                id,
-            )),
-        )
-        .item(
-            "ageR",
-            AttributeValue::N(String::from(
-                "300".to_string(),
-            )),
-        )
-        .item("name", AttributeValue::S(String::from("Dan")));
-    request.send().await?;
-
-    Ok(())
+async fn main() {
+    dynamo_db::perform_dynamodb_operations().await.unwrap();
 }
